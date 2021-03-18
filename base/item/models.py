f68bd1fa76267d9c1e2671b4460fc68911d7a7da
Brandon Hsu <yeet@terpmail.umd.edu>
2021-03-17 22:24:34 -0400

    First object test

--- base/item/models.py
+++ base/item/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
+    idn = models.TextField()
     name = models.TextField()
     description = models.TextField()
     price = models.TextField()
-    seller = models.TextField()
+    image = models.TextField()
     category = models.TextField()
