fffbb112fe5ea6f28565aa891176cb5442158b41
Daniel Lee <daniel@Daniels-MacBook-Pro-2.local>
2021-03-20 14:29:51 -0400

    add items pt1

--- base/pages/views.py
+++ base/pages/views.py
@@ -6,7 +6,42 @@ from items.models import Item
 
 def home_view(request,*args, **kwargs):
     context = {
-        "item1": Item.objects.get(id=1)
+        "item1": Item.objects.get(id=1),
+        "item2": Item.objects.get(id=1),
+        "item3": Item.objects.get(id=1),
+        "item4": Item.objects.get(id=1),
+        "item5": Item.objects.get(id=1),
+        "item6": Item.objects.get(id=1),
+        "item7": Item.objects.get(id=1),
+        "item8": Item.objects.get(id=1),
+        "item9": Item.objects.get(id=1),
+        "item10": Item.objects.get(id=1),
+        "item11": Item.objects.get(id=1),
+        "item12": Item.objects.get(id=1),
+        "item13": Item.objects.get(id=1),
+        "item14": Item.objects.get(id=1),
+        "item15": Item.objects.get(id=1),
+        "item16": Item.objects.get(id=1),
+        "item17": Item.objects.get(id=1),
+        "item18": Item.objects.get(id=1),
+        "item19": Item.objects.get(id=1),
+        "item20": Item.objects.get(id=1),
+        "item21": Item.objects.get(id=1),
+        "item22": Item.objects.get(id=1),
+        "item23": Item.objects.get(id=1),
+        "item24": Item.objects.get(id=1),
+        "item25": Item.objects.get(id=1),
+        "item26": Item.objects.get(id=1),
+        "item27": Item.objects.get(id=1),
+        "item28": Item.objects.get(id=1),
+        "item29": Item.objects.get(id=1),
+        "item30": Item.objects.get(id=1),
+        "item31": Item.objects.get(id=1),
+        "item32": Item.objects.get(id=1),
+        "item33": Item.objects.get(id=1),
+        "item34": Item.objects.get(id=1),
+        "item35": Item.objects.get(id=1),
+        "item36": Item.objects.get(id=1)
     }
     return render(request, "home.html", context)
 
