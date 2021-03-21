dfaa9ffe0f29166b4931acbfcac5e16f61363ed8
Brandon Hsu <yeet@terpmail.umd.edu>
2021-03-20 21:09:14 -0400

    Moved hover.js

---
+++ base/templates/scripts/hover.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var timer;
+
+    $("#im1").hover(
+        function(){
+            sessionStorage.hovertime = 0;
+            document.getElementById("result").innerHTML = sessionStorage.hovertime + " ms";
+        
+            timer = setInterval(function(){
+            if (sessionStorage.hovertime) {
+                sessionStorage.hovertime = Number(sessionStorage.hovertime) + .2;
+            } else {
+                sessionStorage.hovertime = .2;
+            }
+            document.getElementById("result").innerHTML = sessionStorage.hovertime + " ms";
+            }, 200);
+        }, 
+
+        function(){clearInterval(timer)}
+    );
+});
